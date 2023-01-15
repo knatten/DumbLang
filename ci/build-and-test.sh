f8f520d19bda3c2f98c00bb3bd55161624d34481
Anders Schau Knatten <anders@knatten.org>
2023-01-15 14:45:02 +0100

    Add formatting script to CI, format all code
    Run with `--fix` to fix all files, or `--check` to check all files.
    
    Also extracted some commonly used shell variables into common.sh.

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        OS=linux
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        OS=mac
-else
-  echo "Unknown OS $OSTYPE"
-  exit 1
-fi
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ROOT_DIR=$SCRIPT_DIR/..
+source $SCRIPT_DIR/common.sh
+
 BUILD_DIR=$ROOT_DIR/build/ci/$OS
-SRC_DIR=$ROOT_DIR/src
-CI_DIR=$ROOT_DIR/ci
-C_COMPILER=clang-15
-CXX_COMPILER=clang++-15
+
 
 mkdir -p $BUILD_DIR || exit $?
 cd $BUILD_DIR || exit $?
--- ci/setup.sh
+++ ci/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo apt-get update || exit $?
 sudo apt-get install --assume-yes lsb-release software-properties-common || exit $?
 TMP_DIR=$(mktemp -d)
 pushd $TMP_DIR || exit $?
 wget https://apt.llvm.org/llvm.sh || exit $?
 chmod +x llvm.sh || exit $?
 sudo ./llvm.sh 15|| exit $?
-pip install conan || exit %?
+sudo apt install clang-format-15 || exit $?
+pip install conan || exit $?
