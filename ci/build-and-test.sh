014605fe5de08d47052de6011c7866e5163106ba
Anders Schau Knatten <anders@knatten.org>
2023-01-18 12:42:49 +0100

    Conan: Use CMakeDeps generator
    The previous cmake generator is deprecated, let's use CmakeDeps
    instead. CMakeDeps is a multi config generator, so we need to specify
    the build type to CMake for it to work. That's a good idea anyway imo.

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -8,9 +8,9 @@ BUILD_DIR=$ROOT_DIR/build/ci/$OS
 
 mkdir -p $BUILD_DIR || exit $?
 cd $BUILD_DIR || exit $?
-conan install $SRC_DIR --profile=$CI_DIR/conan-profiles/$OS/release  --build=missing || exit $?
-cmake -S $SRC_DIR -B $BUILD_DIR -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER || exit $?
+conan install $SRC_DIR --profile=$CI_DIR/conan-profiles/$OS/release --build=missing || exit $?
+cmake -S $SRC_DIR -B $BUILD_DIR -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER -DCMAKE_BUILD_TYPE=Release || exit $?
 cmake --build $BUILD_DIR || exit $?
 
 ctest || exit $?
-$BUILD_DIR/bin/dumblang ../../../examples/example.dumb || exit $? # Just check that it runs
+$BUILD_DIR/dumblang/dumblang ../../../examples/example.dumb || exit $? # Just check that it runs
